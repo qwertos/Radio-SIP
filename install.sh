2be02c59d1216efe235502da51e865cb94d15f31
Aaron Herting <aaron@herting.cc>
2014-03-01 12:46:30 -0500

    added ruby source install

--- install.sh
+++ install.sh
@@ -9,12 +9,16 @@ NC='\e[0m'
 YELLOW='\e[1;33m'
 
 # LIBRE Location
-LIBRE_SOURCE='http://www.creytiv.com/pub/re-0.4.7.tar.gz'
 LIBRE_VERSION='re-0.4.7'
+LIBRE_SOURCE="http://www.creytiv.com/pub/${LIBRE_VERSION}.tar.gz"
 
 # LIBREM Location
-LIBREM_SOURCE='http://www.creytiv.com/pub/rem-0.4.5.tar.gz'
 LIBREM_VERSION='rem-0.4.5'
+LIBREM_SOURCE="http://www.creytiv.com/pub/${LIBREM_VERSION}.tar.gz"
+
+RUBY_VERSION='2.1.1'
+RUBY_SOURCE="http://cache.ruby-lang.org/pub/ruby/stable/ruby-${RUBY_VERSION}.tar.gz"
+
 
 
 
@@ -95,7 +99,60 @@ install_ruby_gems () {
 }
 
 
+install_build_tools () {
+	if which yum ; then
+		yum -y groupinstall 'Development Tools'
+		return
+	fi
+
+	if which apt-get ; then
+		apt-get -y install build-essential
+		return
+	fi
+}
+
+check_ruby () {
+	echo -en "Checking for ruby... "
+	if which ruby ; then
+		echo -e "[ ${GREEN}OK${NC} ]"
+		echo -en "Checking ruby for version ${CYAN}${RUBY_VERSION}${NC}... "
+		if ruby --verion | grep "^ruby ${RUBY_VERSION}" > /dev/null 2> /dev/null ; then
+			echo -e "[ ${GREEN}OK${NC} ]"
+			return
+		else
+			echo -e "[${RED}FAIL${NC}]"
+			install_ruby
+			return
+		fi
+	else
+		echo -e "[${RED}FAIL${NC}]"
+		install_ruby
+		return
+	fi
+}
+
+
+install_ruby () {
+	echo -e "Installing ruby... "
+	
+	pushd .
+	cd dependencies
+	wget $RUBY_SOURCE
+	tar xvzf ruby-${RUBY_VERSION}.tar.gz
+	cd ruby-${RUBY_VERSION}
+	./configure
+	make
+	make install
+
+	popd
+
+	echo -e "Installing ruby... [ ${GREEN}OK${NC} ]"
+}
+
+
 install_dependencies () {
+	install_build_tools
+	check_ruby
 	install_ruby_gems
 	install_libre
 	install_librem
